697f0ef797f61d41633d1867cbf2f281f898b792
Damianerx <damian@erxilape.com.ar>
2024-03-10 19:15:18 -0300

    +CSS y carga desde un array los item-shops

--- main.js
+++ main.js
@@ -1,82 +1,78 @@
+//CAPTURO ID DEL DOM PARA INSETAR LAS CARDS DE PRODUCTOS
+const carditem = document.getElementById("shopContent")
+
+//ARRAY DE PRODUCTOS
 const productos = [{
-        nombre: "coca",
+        id: 1,
+        nombre: "almohada de la abuela",
         precio: 1000,
+        img: "./assets/img/almohada.png"
     },
     {
-        nombre: "pesi",
+        id: 2,
+        nombre: "carretilla",
         precio: 750,
+        img: "./assets/img/carretilla.png"
     },
     {
-        nombre: "manaos",
+        id: 3,
+        nombre: "disco 80's",
         precio: 500,
+        img: "./assets/img/disco.png"
     },
     {
-        nombre: "naranpol",
+        id: 4,
+        nombre: "liber Libro",
         precio: 250,
+        img: "./assets/img/libro.png"
+    },
+    {
+        id: 5,
+        nombre: "Termo Lumilagro",
+        precio: 250,
+        img: "./assets/img/lumi.png"
+    },
+    {
+        id: 6,
+        nombre: "Papagayo",
+        precio: 250,
+        img: "./assets/img/papagayo.png"
+    },
+    {
+        id: 7,
+        nombre: "Patito",
+        precio: 250,
+        img: "./assets/img/patito.png"
+    },
+    {
+        id: 8,
+        nombre: "Peluca Wolverine",
+        precio: 250,
+        img: "./assets/img/primo.png"
+    },
+    {
+        id: 9,
+        nombre: "Sacacorchos",
+        precio: 250,
+        img: "./assets/img/sacacorchos.png"
     },
 ];
 
+//CREO EL CHANGO DONDE
 let changuito = [];
 
-let seleccion = prompt("Quiere comprar algo señora? (si/no)");
+//RECORRO EL ARRAY DE PRODUCTOS Y CREO UN DIV CON CADA ITEM
+productos.forEach((item) => {
+    let content = document.createElement("div");
+    //LE PONGO LA PROPIEDAD CLASS PARA MAQUILLARLO LUEGO CON CSS
+    content.className = "card-item"
+    content.innerHTML = `
+    <span class="titulo-item">${item.nombre}</span>
+    <img src="${item.img}" alt="" class="img-item">
+    <span class="precio-item">$${item.precio}</span>
+    <button class="boton-item">Agregar al changuito</button>
+    `;
 
-while (seleccion != "si" && seleccion != "no") {
-    alert("debe responder si o no");
-    seleccion = prompt("Quiere comprar algo señora?");
-}
-
-if (seleccion == "si") {
-    alert("a constinuación, nuestras bebidas");
-    let todosLosProductos = productos.map((producto) =>
-        producto.nombre + " $" + producto.precio);
-    alert(todosLosProductos.join("\n"));
-} else {
-    alert("con todo respeto, hágasela plantar señora.")
-}
-
-while (seleccion != "no") {
-    let producto = prompt("que va a llevar para la previa?");
-    let precio = 0;
-
-    if (producto == "coca" ||
-        producto == "pesi" ||
-        producto == "manaos" ||
-        producto == "naranpol") {
-
-        switch (producto) {
-            case "coca":
-                precio = 1000
-                break;
-            case "pesi":
-                precio = 750
-                break;
-            case "manaos":
-                precio = 500
-                break;
-            case "naranpol":
-                precio = 250
-                break;
-        }
-        let unidades = parseInt(prompt(`cuántas ${producto} quiere señora?`));
-
-        changuito.push({ producto, unidades, precio });
-        console.log(changuito);
-    } else {
-        prompt("no tenemos merca señora");
-    }
-
-    seleccion = prompt("alguna otra cosita? si/no");
-
-    while (seleccion == "no") {
-        alert("gracias, vuelva pronto!");
-        changuito.forEach((changuitoFinal) => {
-            console.log(`producto: ${changuitoFinal.producto}, cantidad: ${changuitoFinal.unidades}, TOTAL: $${changuitoFinal.unidades * changuitoFinal.precio}`)
-        })
-        break;
-    }
-}
-
-const total = changuito.reduce((acc, item) => acc + item.precio * item.unidades, 0);
-console.log(`
-    el total a pagar es: $${total}
-`)
+    //INSERTO EN EL DOM CADA ITEM DEL SHOP
+    carditem.append(content);
+});
