29e8e8943d7840fb8e7bc40dc5b0ca56ceb14886
Damianerx <damian@erxilape.com.ar>
2024-03-11 07:41:42 -0300

    agregado array de productos por separado

--- main.js
+++ main.js
@@ -1,64 +1,7 @@
 //CAPTURO ID DEL DOM PARA INSETAR LAS CARDS DE PRODUCTOS
 const carditem = document.getElementById("shopContent")
 
-//ARRAY DE PRODUCTOS
-const productos = [{
-        id: 1,
-        nombre: "almohada de la abuela",
-        precio: 1000,
-        img: "./assets/img/almohada.png"
-    },
-    {
-        id: 2,
-        nombre: "carretilla",
-        precio: 750,
-        img: "./assets/img/carretilla.png"
-    },
-    {
-        id: 3,
-        nombre: "disco 80's",
-        precio: 500,
-        img: "./assets/img/disco.png"
-    },
-    {
-        id: 4,
-        nombre: "liber Libro",
-        precio: 250,
-        img: "./assets/img/libro.png"
-    },
-    {
-        id: 5,
-        nombre: "Termo Lumilagro",
-        precio: 250,
-        img: "./assets/img/lumi.png"
-    },
-    {
-        id: 6,
-        nombre: "Papagayo",
-        precio: 250,
-        img: "./assets/img/papagayo.png"
-    },
-    {
-        id: 7,
-        nombre: "Patito",
-        precio: 250,
-        img: "./assets/img/patito.png"
-    },
-    {
-        id: 8,
-        nombre: "Peluca Wolverine",
-        precio: 250,
-        img: "./assets/img/primo.png"
-    },
-    {
-        id: 9,
-        nombre: "Sacacorchos",
-        precio: 250,
-        img: "./assets/img/sacacorchos.png"
-    },
-];
-
-//CREO EL CHANGO DONDE
+//CREO EL CHANGO DONDE VOY A GUARDAR TODO
 let changuito = [];
 
 //RECORRO EL ARRAY DE PRODUCTOS Y CREO UN DIV CON CADA ITEM
